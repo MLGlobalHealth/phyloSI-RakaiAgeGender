3b51651d281640af4ef163c270ee384b2d0fc728
Mélodie MONOD <monod.melodie@gmail.com>
2021-12-07 18:46:57 +0000

    minor fixes

--- run_stan.sh
+++ run_stan.sh
@@ -3,12 +3,12 @@
 JOBID=$$
 STAN_MODEL="gp_211207"
 TAG="MRC_FALSE_OnlyHTX_TRUE_threshold_0.5"
-CWD="/rds/general/user/mm3218/home/projects/2021/phyloflows"/$TAG
+DATADIR="/rds/general/user/mm3218/home/projects/2021/phyloflows"/$TAG
 INDIR="/rds/general/user/mm3218/home/git/phyloflows"
 
-mkdir $CWD
+mkdir $DATADIR
 
-cat > $CWD/bash_$STAN_MODEL-$JOBID.pbs <<EOF
+cat > $DATADIR/bash_$STAN_MODEL-$JOBID.pbs <<EOF
   
 #!/bin/sh
 #PBS -l walltime=72:00:00
@@ -20,8 +20,8 @@ JOB_TEMP=\${EPHEMERAL}/\${PBS_JOBID}
 mkdir -p \$JOB_TEMP
 cd \$JOB_TEMP  
 PWD=\$(pwd)
-CWD=$CWD
-INDIR=$INDIR/inst
+DATADIR=$DATADIR
+INDIR=$INDIR
 STAN_MODEL=$STAN_MODEL
 JOBID=$JOBID
 TAG=$TAG
@@ -29,46 +29,20 @@ TAG=$TAG
 # main directory
 mkdir \$PWD/\$STAN_MODEL-\$JOBID
   
-# directories for fits, figures and tables
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/fits
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/data
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/figure
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/table
+Rscript \$INDIR/scripts/run_stan.R -indir \$INDIR -datadir \$DATADIR -outdir \$PWD -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
   
-Rscript \$INDIR/scripts/run_stan.R -indir \$INDIR -outdir \$PWD -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
+cp -R --no-preserve=mode,ownership \$PWD/* \$DATADIR
   
-cp -R --no-preserve=mode,ownership \$PWD/* \$CWD
-  
-cd \$CWD
+cd \$DATADIR
 qsub bash_\$STAN_MODEL-\$JOBID-postprocessing.pbs
 
 EOF
 
-cat > $CWD/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
-  
-#!/bin/sh
-#PBS -l walltime=40:59:00
-#PBS -l select=1:ncpus=10:ompthreads=1:mem=240gb
-#PBS -j oe
-module load anaconda3/personal
-  
-CWD=$CWD
-PWD=$CWD
-INDIR=$INDIR/inst
-STAN_MODEL=$STAN_MODEL
-JOBID=$JOBID
-TAG=$TAG
-  
-# main directory
-mkdir \$PWD/\$STAN_MODEL-\$JOBID
-  
-# directories for fits, figures and tables
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/figure
-mkdir \$PWD/\$STAN_MODEL-\$JOBID/table
+cat > $DATADIR/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
   
 # to write ....
 EOF
 
   
-cd $CWD
+cd $DATADIR
 qsub bash_$STAN_MODEL-$JOBID.pbs
--- scripts/run_stan.R
+++ scripts/run_stan.R
@@ -1,56 +1,70 @@
 library(rstan)
 library(data.table)	
 
-.indir <- '/rds/general/user/mm3218/home/git/phyloflows'
-.outdir <- '/rds/general/user/mm3218/home/projects/2021/phyloflows'
+lab <- "MRC_FALSE_OnlyHTX_TRUE_threshold_0.5"
+.stan_model <- 'gp_211207'
+DEBUG <- F
+JOBID = 12
+
+.indir <- "/rds/general/user/mm3218/home/git/phyloflows"
+datadir <- "/rds/general/user/mm3218/home/projects/2021/phyloflows"
 
 if(0){
   .indir <- '~/git/phyloflows'
-  .outdir <- '~/Box\ Sync/2021/RCCS/outputs'
+  datadir <- '~/Box\ Sync/2021/phyloflows/'
 }
 
-lab <- "MRC_FALSE_OnlyHTX_TRUE_threshold_0.5"
-stan_model <- 'gp_211207'
-DEBUG <- F
+.outdir <- file.path(datadir, lab)
 
 args_line <-  as.list(commandArgs(trailingOnly=TRUE))
 print(args_line)
 if(length(args_line) > 0)
 {
   stopifnot(args_line[[1]]=='-indir')
-  stopifnot(args_line[[3]]=='-outdir')
-  stopifnot(args_line[[5]]=='-stan_model')
-  stopifnot(args_line[[7]]=='-JOBID')
-  stopifnot(args_line[[9]]=='-lab')
+  stopifnot(args_line[[3]]=='-datadir')
+  stopifnot(args_line[[5]]=='-outdir')
+  stopifnot(args_line[[7]]=='-stan_model')
+  stopifnot(args_line[[9]]=='-JOBID')
+  stopifnot(args_line[[11]]=='-lab')
   .indir <- args_line[[2]]
-  .outdir <- args_line[[4]]
-  stan_model <- args_line[[6]]
-  JOBID <- as.numeric(args_line[[8]])
-  lab <- as.numeric(args_line[[10]])
+  datadir <- args_line[[4]]
+  .outdir <- args_line[[6]]
+  .stan_model <- args_line[[8]]
+  .JOBID <- args_line[[10]]
+  lab <- args_line[[12]]
 }
 
-path.to.stan.data <- file.path(.outdir, lab, paste0("stanin_",lab,".RData"))
+path.to.stan.data <- file.path(datadir, paste0("stanin_",lab,".RData"))
 load(path.to.stan.data)
 
-indir <- .indir; outdir <- .outdir; outdir.lab <- file.path(outdir, lab)
-path.to.stan.model <- file.path(indir, 'stan_models', paste0(stan_model, '.stan'))
+indir <- .indir; outdir <- .outdir; 
+path.to.stan.model <- file.path(indir, 'stan_models', paste0(.stan_model, '.stan'))
 
 # run stan model
 model = rstan::stan_model(path.to.stan.model)
 
+stan_init <- list()
+stan_init$rho_gp1 = rep(1.25, stan_data$N_group)
+stan_init$rho_gp2 = rep(1.25, stan_data$N_group)
+stan_init$rho_gp3 = rep(1.25, stan_data$N_group)
+stan_init$nu = 0
+stan_init$mu = 0
+
 if(DEBUG){
-  fit <- sampling(model, data = stan_data, iter = 100, warmup = 50, chains=1, thin=1)
+  fit <- sampling(model, data = stan_data, iter = 10, warmup = 5, chains=1, thin=1, init=rep(list(stan_init),1))
 }else{
   options(mc.cores = parallel::detectCores())
   rstan_options(auto_write = TRUE)
   fit <- sampling(model, data = stan_data,
                   iter = 3000, warmup = 500, chains=4, thin=1, seed = 5,
-                  verbose = FALSE, control = list(adapt_delta = 0.999,max_treedepth=15))
+                  verbose = FALSE, control = list(adapt_delta = 0.999,max_treedepth=15),
+                  init=rep(list(stan_init), 4))
 }
 
 sum = summary(fit)
 sum$summary[which(sum$summary[,9] < 100),]
 
-file = file.path(outdir.lab, paste0(stan_model,'-', JOBID, '_', lab, '.rds'))
+file = file.path(outdir, paste0(.stan_model,'-', .JOBID), paste0(.stan_model,'-', .JOBID, '_', lab, '.rds'))
+cat("Save file ", file)
 saveRDS(fit,file = file)
      
