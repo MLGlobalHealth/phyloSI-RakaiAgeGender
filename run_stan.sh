6b2aa3676ca6b10b165521152063991390b6aa1f
Mélodie MONOD <monod.melodie@gmail.com>
2021-12-08 14:48:50 +0000

    shell to start runs

--- run_stan.sh
+++ run_stan.sh
@@ -34,15 +34,37 @@ Rscript \$INDIR/scripts/run_stan.R -indir \$INDIR -datadir \$DATADIR -outdir \$P
 cp -R --no-preserve=mode,ownership \$PWD/* \$DATADIR
   
 cd \$DATADIR
-qsub bash_\$STAN_MODEL-\$JOBID-postprocessing.pbs
+qsub bash_$STAN_MODEL-$JOBID-postprocessing.pbs
 
 EOF
 
 cat > $DATADIR/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
   
-# to write ....
-EOF
+#!/bin/sh
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:ompthreads=1:mem=120gb
+#PBS -j oe
+module load anaconda3/personal
 
+JOB_TEMP=\${EPHEMERAL}/\${PBS_JOBID}
+mkdir -p \$JOB_TEMP
+cd \$JOB_TEMP  
+PWD=\$(pwd)
+DATADIR=$DATADIR
+INDIR=$INDIR
+STAN_MODEL=$STAN_MODEL
+JOBID=$JOBID
+TAG=$TAG
+
+# directories for figure and table
+mkdir \$PWD/figures
+mkdir \$PWD/tables
+
+Rscript \$INDIR/scripts/postprocessing_results.R -indir \$INDIR -datadir \$DATADIR -outdir \$PWD -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
+
+cp -R --no-preserve=mode,ownership \$PWD/* \$DATADIR/\$STAN_MODEL-\$JOBID
+
+EOF
   
 cd $DATADIR
 qsub bash_$STAN_MODEL-$JOBID.pbs
