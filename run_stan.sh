503849f3f63eb91c7855c0847289a0080a11a308
Mélodie MONOD <monod.melodie@gmail.com>
2022-03-14 10:59:30 +0000

    merge process data and run stan

--- run_stan.sh
+++ run_stan.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-JOBID=$$
-STAN_MODEL="gp_211117"
-TAG="MRC_FALSE_OnlyHTX_TRUE_threshold_0.5_jobname_firstruncutoff"
-DATADIR="/rds/general/user/mm3218/home/projects/2021/phyloflows"/$TAG
+STAN_MODEL="gp_220108"
+JOBNAME="cutoff_2014"
 INDIR="/rds/general/user/mm3218/home/git/phyloflows"
+OUTDIR="/rds/general/user/mm3218/home/projects/2021/phyloflows"
 
-mkdir $DATADIR
+mkdir $OUTDIR
 
-cat > $DATADIR/bash_$STAN_MODEL-$JOBID.pbs <<EOF
+cat > $OUTDIR/bash_$STAN_MODEL-$JOBID.pbs <<EOF
   
 #!/bin/sh
-#PBS -l walltime=72:00:00
+#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=10:ompthreads=1:mem=240gb
 #PBS -j oe
 module load anaconda3/personal
@@ -20,27 +19,26 @@ JOB_TEMP=\${EPHEMERAL}/\${PBS_JOBID}
 mkdir -p \$JOB_TEMP
 cd \$JOB_TEMP  
 PWD=\$(pwd)
-OUTDIR=\$PWD/\$STAN_MODEL-\$JOBID
-DATADIR=$DATADIR
+
 INDIR=$INDIR
+OUTDIR=$OUTDIR
 STAN_MODEL=$STAN_MODEL
-JOBID=$JOBID
-TAG=$TAG
+JOBNAME=$JOBNAME
   
 # main directory
-mkdir \$OUTDIR
+mkdir \$PWD/\$STAN_MODEL-\$JOBID
   
-Rscript \$INDIR/scripts/process_data.R -indir \$INDIR -datadir \$DATADIR -outdir \$OUTDIR -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
-Rscript \$INDIR/scripts/run_stan.R -indir \$INDIR -datadir \$DATADIR -outdir \$OUTDIR -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
+Rscript \$INDIR/scripts/process_data.R -indir \$INDIR -outdir \$PWD -stan_model \$STAN_MODEL -jobname \$JOBNAME
+Rscript \$INDIR/scripts/run_stan.R -indir \$INDIR -outdir \$PWD -stan_model \$STAN_MODEL -jobname \$JOBNAME
   
-cp -R --no-preserve=mode,ownership \$PWD/* \$DATADIR
+cp -R --no-preserve=mode,ownership \$PWD/* \$OUTDIR
   
-cd \$DATADIR
+cd \$OUTDIR
 qsub bash_$STAN_MODEL-$JOBID-postprocessing.pbs
 
 EOF
 
-cat > $DATADIR/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
+cat > $OUTDIR/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
   
 #!/bin/sh
 #PBS -l walltime=24:00:00
@@ -48,25 +46,17 @@ cat > $DATADIR/bash_$STAN_MODEL-$JOBID-postprocessing.pbs <<EOF
 #PBS -j oe
 module load anaconda3/personal
 
-JOB_TEMP=\${EPHEMERAL}/\${PBS_JOBID}
-mkdir -p \$JOB_TEMP
-cd \$JOB_TEMP  
-PWD=\$(pwd)
-DATADIR=$DATADIR
 INDIR=$INDIR
 STAN_MODEL=$STAN_MODEL
-JOBID=$JOBID
-TAG=$TAG
-
+JOBNAME=$JOBNAME
+  
 # directories for figure and table
-mkdir \$PWD/figures
-mkdir \$PWD/tables
+mkdir \$PWD/\$STAN_MODEL-\$JOBID/figures
+mkdir \$PWD/\$STAN_MODEL-\$JOBID/tables
 
-Rscript \$INDIR/scripts/postprocessing_results.R -indir \$INDIR -datadir \$DATADIR -outdir \$PWD -stan_model \$STAN_MODEL -JOBID \$JOBID -lab \$TAG
-
-cp -R --no-preserve=mode,ownership \$PWD/* \$DATADIR/\$STAN_MODEL-\$JOBID
+Rscript \$INDIR/scripts/postprocessing_results.R -indir \$INDIR -outdir \$PWD -stan_model \$STAN_MODEL -jobname \$JOBNAME 
 
 EOF
   
-cd $DATADIR
+cd $OUTDIR
 qsub bash_$STAN_MODEL-$JOBID.pbs
