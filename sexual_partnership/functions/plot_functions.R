c1ccb0fde16b85ebe569cc2eca1ed36a12d62a86
YuCHENJT <Sweet-Y-J@outlook.com>
2022-05-12 22:07:59 +0100

    update work on 7 May

--- sexual_partnership/functions/plot_functions.R
+++ sexual_partnership/functions/plot_functions.R
@@ -16,6 +16,21 @@ plot_contact_intensity_matrix = function(d22)
     theme_bw() +
     theme(legend.position = 'bottom')
 }
+plot_contact_intensity_sd_matrix = function(d22)
+{
+  set(d22, NULL,"gender_label", d22[, paste0(part.sex,' (Participant)')])
+  set(d22, NULL,"alter_gender_label", d22[, paste0(cont.sex,' (Contacted)')])
+
+  ggplot(d22) +
+    geom_tile(aes(x = part.age, y = cont.age, fill = sd)) +
+    coord_equal() +
+    facet_grid(alter_gender_label ~ gender_label) +
+    scale_fill_continuous(type = "viridis", na.value = 'transparent') +
+    # scale_fill_gradient2(low = "darkblue", high = "darkgreen", guide = "colorbar", na.value = 'white'
+    # ) +
+    theme_bw() +
+    theme(legend.position = 'bottom')
+}
 
 plot_contact_intensity_matrix_emprical = function(d22)
 {
@@ -33,8 +48,57 @@ plot_contact_intensity_matrix_emprical = function(d22)
     theme(legend.position = 'bottom')
 }
 
-# plot posterior age marginal contact intensities distirbution----
-plot_age_marginal_contact_intensity_MF = function(pred_cntct)
+# plot four panels contact rate patterns----
+plot_contact_rate_matrix = function(d22)
+{
+  set(d22, NULL,"gender_label", d22[, paste0(part.sex,' (Participant)')])
+  set(d22, NULL,"alter_gender_label", d22[, paste0(cont.sex,' (Contacted)')])
+
+  ggplot(d22) +
+    geom_tile(aes(x = part.age, y = cont.age, fill = M)) +
+    coord_equal() +
+    facet_grid(alter_gender_label ~ gender_label) +
+    scale_fill_continuous(type = "viridis", na.value = 'transparent') +
+    # scale_fill_gradient2(low = "darkblue", high = "darkgreen", guide = "colorbar", na.value = 'white'
+    # ) +
+    theme_bw() +
+    theme(legend.position = 'bottom')
+}
+
+plot_contact_rate_sd_matrix = function(d22)
+{
+  set(d22, NULL,"gender_label", d22[, paste0(part.sex,' (Participant)')])
+  set(d22, NULL,"alter_gender_label", d22[, paste0(cont.sex,' (Contacted)')])
+
+  ggplot(d22) +
+    geom_tile(aes(x = part.age, y = cont.age, fill = sd)) +
+    coord_equal() +
+    facet_grid(alter_gender_label ~ gender_label) +
+    scale_fill_continuous(type = "viridis", na.value = 'transparent') +
+    # scale_fill_gradient2(low = "darkblue", high = "darkgreen", guide = "colorbar", na.value = 'white'
+    # ) +
+    theme_bw() +
+    theme(legend.position = 'bottom')
+}
+plot_contact_rate_matrix_emprical = function(d22)
+{
+  set(d22, NULL,"gender_label", d22[, paste0(part.sex,' (Participant)')])
+  set(d22, NULL,"alter_gender_label", d22[, paste0(cont.sex,' (Contacted)')])
+
+  ggplot(d22) +
+    geom_tile(aes(x = part.age, y = cont.age, fill = M)) +
+    coord_equal() +
+    facet_grid(alter_gender_label ~ label + gender_label) +
+    scale_fill_continuous(type = "viridis", na.value = 'transparent') +
+    # scale_fill_gradient2(low = "darkblue", high = "darkgreen", guide = "colorbar", na.value = 'white'
+    # ) +
+    theme_bw() +
+    theme(legend.position = 'bottom')
+}
+
+
+# plot posterior age contact intensities distribution----
+plot_age_contact_intensity_MF = function(pred_cntct)
 {
   pred_cntct <- subset(pred_cntct,
                        grepl('M', part.sex) &
@@ -65,7 +129,7 @@ plot_age_marginal_contact_intensity_MF = function(pred_cntct)
   return(p_m)
 }
 
-plot_age_marginal_contact_intensity_FM = function(pred_cntct)
+plot_age_contact_intensity_FM = function(pred_cntct)
 {
   pred_cntct <- subset(pred_cntct,
                        grepl('F', part.sex) &
@@ -96,7 +160,7 @@ plot_age_marginal_contact_intensity_FM = function(pred_cntct)
   return(p_m)
 }
 
-plot_age_contact_intensity_MF = function(pred_cntct)
+plot_age_contact_intensity_MF_empirical = function(pred_cntct)
 {
   pred_cntct <- subset(pred_cntct,
                        grepl('M', part.sex) &
@@ -128,7 +192,7 @@ plot_age_contact_intensity_MF = function(pred_cntct)
   return(p_m)
 }
 
-plot_age_contact_intensity_FM = function(pred_cntct)
+plot_age_contact_intensity_FM_empirical = function(pred_cntct)
 {
   pred_cntct <- subset(pred_cntct,
                        grepl('F', part.sex) &
@@ -152,6 +216,133 @@ plot_age_contact_intensity_FM = function(pred_cntct)
                                 colour = "transparent", show.legend = FALSE) +
     geom_step(aes(y = M)) +
     geom_step(aes(y = cntct_intensity_empirical), col = "black", linetype = "dashed") +
+    facet_wrap(~ part.age, ncol = ncol_nb, scales = "free_y") +
+    theme_bw() +
+    guides(fill = "none", scales = "free_y") + theme(legend.position = "none") +
+    xlab("Age of contacted individuals") +
+    ylab("Estimated contact intensities with 95% ci")
+  return(p_m)
+}
+
+# plot posterior age contact rate distrbution ----
+plot_age_contact_rate_MF = function(pred_cntct)
+{
+  pred_cntct <- subset(pred_cntct,
+                       grepl('M', part.sex) &
+                         grepl('F', cont.sex)
+  )
+  if (!is.integer(pred_cntct$part.age)) {
+    pred_cntct$age1 = as.numeric(pred_cntct$part.age - 0.5)
+    pred_cntct$age2 = as.numeric(pred_cntct$part.age + 0.5)
+    pred_cntct$age1 = ifelse(pred_cntct$age1 < 10, paste0('0',pred_cntct$age1), pred_cntct$age1)
+    pred_cntct$age2 = ifelse(pred_cntct$age2 < 10, paste0('0',pred_cntct$age2), pred_cntct$age2)
+    pred_cntct[, part.age := paste0("participants' age:", age1, '-', age2)]
+    ncol_nb = 6
+  }else{
+    pred_cntct$part.age = ifelse(pred_cntct$part.age < 10, paste0('0',pred_cntct$part.age), pred_cntct$part.age)
+    pred_cntct$part.age = as.character(pred_cntct$part.age)
+    pred_cntct[, part.age := paste0("participants' age:", part.age)]
+    ncol_nb = 8
+  }
+  p_m <- ggplot(pred_cntct, aes(x = cont.age,col = part.age)) +
+    pammtools:::geom_stepribbon(aes(ymin = CL, ymax = CU, fill = part.age), linetype = "dotted", alpha = 0.2,
+                                colour = "transparent", show.legend = FALSE) +
+    geom_step(aes(y = M)) +
+    facet_wrap(~ part.age, ncol = ncol_nb, scales = "free_y") +
+    theme_bw() +
+    guides(fill = "none", scales = "free_y") + theme(legend.position = "none") +
+    xlab("Age of contacted individuals") +
+    ylab("Estimated contact intensities with 95% ci")
+  return(p_m)
+}
+
+plot_age_contact_rate_FM = function(pred_cntct)
+{
+  pred_cntct <- subset(pred_cntct,
+                       grepl('F', part.sex) &
+                         grepl('M', cont.sex)
+  )
+  if (!is.integer(pred_cntct$part.age)) {
+    pred_cntct$age1 = as.numeric(pred_cntct$part.age - 0.5)
+    pred_cntct$age2 = as.numeric(pred_cntct$part.age + 0.5)
+    pred_cntct$age1 = ifelse(pred_cntct$age1 < 10, paste0('0',pred_cntct$age1), pred_cntct$age1)
+    pred_cntct$age2 = ifelse(pred_cntct$age2 < 10, paste0('0',pred_cntct$age2), pred_cntct$age2)
+    pred_cntct[, part.age := paste0("participants' age:", age1, '-', age2)]
+    ncol_nb = 6
+  }else{
+    pred_cntct$part.age = ifelse(pred_cntct$part.age < 10, paste0('0',pred_cntct$part.age), pred_cntct$part.age)
+    pred_cntct$part.age = as.character(pred_cntct$part.age)
+    pred_cntct[, part.age := paste0("participants' age:", part.age)]
+    ncol_nb = 8
+  }
+  p_m <- ggplot(pred_cntct, aes(x = cont.age,col = part.age)) +
+    pammtools:::geom_stepribbon(aes(ymin = CL, ymax = CU, fill = part.age), linetype = "dotted", alpha = 0.2,
+                                colour = "transparent", show.legend = FALSE) +
+    geom_step(aes(y = M)) +
+    facet_wrap(~ part.age, ncol = ncol_nb, scales = "free_y") +
+    theme_bw() +
+    guides(fill = "none", scales = "free_y") + theme(legend.position = "none") +
+    xlab("Age of contacted individuals") +
+    ylab("Estimated contact intensities with 95% ci")
+  return(p_m)
+}
+
+plot_age_contact_rate_MF_empirical = function(pred_cntct)
+{
+  pred_cntct <- subset(pred_cntct,
+                       grepl('M', part.sex) &
+                         grepl('F', cont.sex)
+  )
+  if (!is.integer(pred_cntct$part.age)) {
+    pred_cntct$age1 = as.numeric(pred_cntct$part.age - 0.5)
+    pred_cntct$age2 = as.numeric(pred_cntct$part.age + 0.5)
+    pred_cntct$age1 = ifelse(pred_cntct$age1 < 10, paste0('0',pred_cntct$age1), pred_cntct$age1)
+    pred_cntct$age2 = ifelse(pred_cntct$age2 < 10, paste0('0',pred_cntct$age2), pred_cntct$age2)
+    pred_cntct[, part.age := paste0("participants' age:", age1, '-', age2)]
+    ncol_nb = 6
+  }else{
+    pred_cntct$part.age = ifelse(pred_cntct$part.age < 10, paste0('0',pred_cntct$part.age), pred_cntct$part.age)
+    pred_cntct$part.age = as.character(pred_cntct$part.age)
+    pred_cntct[, part.age := paste0("participants' age:", part.age)]
+    ncol_nb = 8
+  }
+  p_m <- ggplot(pred_cntct, aes(x = cont.age,col = part.age)) +
+    pammtools:::geom_stepribbon(aes(ymin = CL, ymax = CU, fill = part.age), linetype = "dotted",alpha = 0.2,
+                                colour = "transparent", show.legend = FALSE) +
+    geom_step(aes(y = M)) +
+    geom_step(aes(y = cntct_rate_empirical), col = "black", linetype = "dashed") +
+    facet_wrap(~ part.age, ncol = ncol_nb, scales = "free_y") +
+    theme_bw() +
+    guides(fill = "none", scales = "free_y") + theme(legend.position = "none") +
+    xlab("Age of contacted individuals") +
+    ylab("Estimated contact intensities with 95% ci")
+  return(p_m)
+}
+
+plot_age_contact_rate_FM_empirical = function(pred_cntct)
+{
+  pred_cntct <- subset(pred_cntct,
+                       grepl('F', part.sex) &
+                         grepl('M', cont.sex)
+  )
+  if (!is.integer(pred_cntct$part.age)) {
+    pred_cntct$age1 = as.numeric(pred_cntct$part.age - 0.5)
+    pred_cntct$age2 = as.numeric(pred_cntct$part.age + 0.5)
+    pred_cntct$age1 = ifelse(pred_cntct$age1 < 10, paste0('0',pred_cntct$age1), pred_cntct$age1)
+    pred_cntct$age2 = ifelse(pred_cntct$age2 < 10, paste0('0',pred_cntct$age2), pred_cntct$age2)
+    pred_cntct[, part.age := paste0("participants' age:", age1, '-', age2)]
+    ncol_nb = 6
+  }else{
+    pred_cntct$part.age = ifelse(pred_cntct$part.age < 10, paste0('0',pred_cntct$part.age), pred_cntct$part.age)
+    pred_cntct$part.age = as.character(pred_cntct$part.age)
+    pred_cntct[, part.age := paste0("participants' age:", part.age)]
+    ncol_nb = 8
+  }
+  p_m <- ggplot(pred_cntct, aes(x = cont.age,col = part.age)) +
+    pammtools:::geom_stepribbon(aes(ymin = CL, ymax = CU, fill = part.age), linetype = "dotted",alpha = 0.2,
+                                colour = "transparent", show.legend = FALSE) +
+    geom_step(aes(y = M)) +
+    geom_step(aes(y = cntct_rate_empirical), col = "black", linetype = "dashed") +
     facet_wrap(~ part.age, ncol = ncol_nb, scales = "free_y") +
     theme_bw() +
     guides(fill = "none", scales = "free_y") + theme(legend.position = "none") +
