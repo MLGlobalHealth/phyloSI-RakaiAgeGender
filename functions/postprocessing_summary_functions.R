72fa48a209ca409fb493f9cc0cc2c9328b52aab1
Mélodie MONOD <monod.melodie@gmail.com>
2022-12-15 14:18:33 +0000

    minor

--- functions/postprocessing_summary_functions.R
+++ functions/postprocessing_summary_functions.R
@@ -304,6 +304,9 @@ find_summary_output_by_round <- function(samples, output, vars,
     # take median age source
     setnames(tmp1, 'value', 'delta')
     vars <- standardised.vars
+    
+    stopifnot(all(tmp1[, list(sum_delta = sum(delta)), by = c('iterations', vars)]$sum_delta - 1 < 1e-10))
+    
     tmp1 <- tmp1[, list(value = matrixStats::weightedMedian(AGE_TRANSMISSION.SOURCE, delta ), 
                         quantile = c('C50')), by = c('iterations', vars)]
     vars = c(vars, 'quantile')
@@ -313,6 +316,9 @@ find_summary_output_by_round <- function(samples, output, vars,
     # take quantile of the age of the source
     setnames(tmp1, 'value', 'delta')
     vars <- standardised.vars
+    
+    stopifnot(all(tmp1[, list(sum_delta = sum(delta)), by = c('iterations', vars)]$sum_delta - 1 < 1e-10))
+    
     tmp1 <- tmp1[, list(value = wquantile(x = AGE_TRANSMISSION.SOURCE, weights = delta,
                                                         probs = c(0.1, 0.25, 0.5, 0.75, 0.9)),
                         quantile = c('C10', 'C25', 'C50', 'C75', 'C90')), by = c('iterations', vars)]
@@ -329,6 +335,9 @@ find_summary_output_by_round <- function(samples, output, vars,
     # take quantile of the difference male age - female age
     setnames(tmp1, 'value', 'delta')
     vars <- standardised.vars
+    
+    stopifnot(all(tmp1[, list(sum_delta = sum(delta)), by = c('iterations', vars)]$sum_delta - 1 < 1e-10))
+    
     tmp1 <- tmp1[, list(value = wquantile(x = (AGE_INFECTION.MALE - AGE_INFECTION.FEMALE), weights = delta,
                                                     probs = c(0.1, 0.25, 0.5, 0.75, 0.9)),
                         quantile = c('C10', 'C25', 'C50', 'C75', 'C90')), by = c('iterations', vars)]
