6356587cfc7c912fda107ef3507e596ecdfe622a
YuCHENJT <43706222+YuCHENJT@users.noreply.github.com>
2023-01-16 17:29:00 +0000

    add code for extended data fig8

---
+++ src/get_extended_data_fig8.R
@@ -0,0 +1,252 @@
+# indir repository
+indir <- '~/git/phyloflows/' # please modify accordingly
+
+# outdir where you stored the transmission flow data
+outdir <- file.path( '~/Box\ Sync/2021/phyloflows/', paste0(stan_model, '-', jobname)) # please modify accordingly
+
+# paths to data
+file.contribution.sexual.contacts <- file.path(indir, 'data', paste0('age_dist_inlandR015.rds')) # please modify accordingly
+file.unsuppressed.share <- file.path(indir, 'fit', paste0('RCCS_unsuppressed_share_sex_221208.csv')) # please modify accordingly
+file.path.round.timeline <- file.path(indir, 'data', 'RCCS_round_timeline_220905.RData') # please modify accordingly
+file.expected_contribution_age_source <- paste0(outdir.table, '-output-log_lambda_latentby_direction_round_age_transmission.sourcestandardisedby_direction_round.rds') # please modify accordingly
+
+# indir to store data
+dir.create(outdir, showWarnings = F)
+outfile <- file.path(outdir, paste0(stan_model,'-', jobname))
+outfile.figures <- file.path(outdir, paste0(stan_model,'-', jobname))
+outdir.table <- file.path(outdir, paste0(stan_model,'-', jobname))
+
+# load functions
+source(file.path(indir, 'functions', 'postprocessing_summary_functions.R'))
+source(file.path(indir, 'functions', 'postprocessing_plot_functions.R'))
+source(file.path(indir, 'functions', 'postprocessing_utils_functions.R'))
+source(file.path(indir, 'functions', 'postprocessing_statistics_functions.R'))
+source(file.path(indir, 'functions', 'summary_functions.R'))
+
+# get fig A ----
+# map direction
+df_direction <- get.df.direction()
+df_community <- get.df.community()
+load(file.path.round.timeline)
+df_round <- make.df.round(df_round_inland)
+
+# load data
+unsuppressed_share <- fread(file.unsuppressed.share) # share of unsuppressed count by sex
+expected_contribution_age_source <- as.data.table(readRDS(file.expected_contribution_age_source))
+
+# load the sexual contact data----
+contribution_sexual_contact <- as.data.table(readRDS(file.path('results', 'modelb_0112', 'NegBin_HSGP-M-20-c-1.5-kernel-matern52-area-inland-R015_age-dist_ma_cntct_area_1549.rds')))
+
+# clean
+df_contribution_sexual_contact <- clean_contribution_sexual_contact_yu(contribution_sexual_contact)
+unsuppressed_share_age <- prepare_unsuppressed_share(unsuppressed_share, c('SEX', 'AGEYRS'), 'SEX')
+
+# plot A----
+# contribution to incidence
+cti <- copy(expected_contribution_age_source)
+
+# contribution to sexual contact
+cts <- copy(df_contribution_sexual_contact)
+
+# contribution to individual with unsuppressed virus
+ctu <- copy(unsuppressed_share_age)
+
+# select one round
+Round <- 'R018'
+cti <- cti[ROUND == Round]
+ctu <- ctu[ROUND == Round]
+
+label.cont.inc <- 'Contribution to HIV incidence'
+label.cont.uns <- 'Contribution to individuals with unsuppressed virus'
+label.cont.cont <- 'Contribution to sexual contacts'
+# plot
+communities <- cti[, unique(COMM)]
+Comm <- communities
+
+cti.c <- cti[COMM == Comm]
+ctu.c <- ctu[COMM == Comm]
+setnames(cti.c, 'AGE_TRANSMISSION.SOURCE', 'AGEYRS')
+setnames(ctu.c, 'LABEL_GENDER_SOURCE', 'SEX')
+ctu.c[, label := 'Age contribution with unsuppressed HIV']
+setnames(cti.c, 'LABEL_GENDER_SOURCE', 'SEX')
+cti.c[, gender := ifelse(LABEL_GENDER_RECIPIENT == 'Women','male', 'female')]
+ctu.c[, gender2 := ifelse(LABEL_GENDER_RECIPIENT == 'Women', 'men', 'women')]
+cts[, gender := ifelse(LABEL_GENDER_RECIPIENT == 'Women', 'male', 'female')]
+ctu.c[, gender := ifelse(LABEL_GENDER_RECIPIENT == 'Women','male', 'female')]
+
+
+cti.c[, label := paste0('Contribution to ', gender ,  '\ntransmitting partners   ' )]
+ctu.c[, label := paste0('Contribution to ', gender2, '\nwith unsuppressed HIV   ')]
+cts[, label := paste0('Contribution to ', gender, '\nsexual contacts   ')]
+
+pltA <- rbind(cti.c[, list(AGEYRS,M,CL,CU,LABEL_SOURCE, label, gender)],
+              ctu.c[, list(AGEYRS,M,CL,CU,LABEL_SOURCE, label, gender)],
+              cts[, list(AGEYRS,M,CL,CU,LABEL_SOURCE, label, gender)])
+# plt
+setkey(pltA, LABEL_SOURCE)
+pltA[, facet_title := ifelse(gender == 'female', 'Women', 'Men')]
+pA <- ggplot(pltA, aes(x = AGEYRS, y = M)) +
+  geom_line(aes(y = M, col = factor(label, levels = unique(label)), linetype = factor(label, levels = unique(label)))) +
+  geom_ribbon(aes(ymin = CL, ymax = CU, fill = factor(label, levels = unique(label))), alpha = .3) +
+  facet_grid(.~factor(facet_title, levels = c('Women', 'Men'))) +
+  scale_color_manual(values = c("#ae017e",'grey50', '#cc4c02',"lightblue4",'grey50' ,'#41ab5d')) +
+  scale_fill_manual(values = c("#fa9fb5",'grey70','#ec7014',"lightblue3",'grey50', '#78c679' )) +
+  scale_x_continuous(breaks = seq(15, 50, 5)) +
+  theme_bw() +
+  labs(x = 'Female age                                                               Male age',
+       y = 'Percent') +
+  theme(
+    strip.background = element_blank(),
+    # strip.background = element_rect(colour="white", fill="white"),
+    strip.text = element_text(size = 16),
+    legend.title = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.text = element_text(size = 9),
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 16)) +
+  theme(
+    legend.position = c(0.695, 1.03),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    # legend.margin = margin(6, 6, 6, 6),
+    legend.background = element_blank()) +
+  scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, .05)), limits = c(0, 0.08)) +
+  scale_linetype_manual(values = c(1,1, 2, 1, 1, 2)) +
+  guides(color = guide_legend(nrow = 3, byrow = F))
+
+pA
+
+
+
+# conditional contact intensities----
+pd.a <- as.data.table(readRDS(file.path('results' ,'modelb_0112', 'NegBin_HSGP-M-20-c-1.5-kernel-matern52-area-inland-R015_age-dist_cntct_area_1549.rds')))
+
+pd.a <- pd.a[cont.age %in% c(15, 20, 25, 30, 35, 40)]
+pd.a$part.sex <- ifelse(pd.a$part.sex == 'F', 'Women', 'Men')
+pd.a$cont.sex <- ifelse(pd.a$part.sex == 'Women',  'Men', 'Women')
+pd.a <- pd.a[, label := ifelse(cont.age %in% c(15, 20, 25), '15, 20, 25 years', '30, 35, 40 years')]
+pd.a[, plt.bar := (part.age - cont.age) %in% c( -10, -5, 0,  5,  15)]
+pd.a[, plt.age := cont.age]
+pd.a[, cont.age := paste0('Sexual contact intensities to ', cont.sex, ' aged ', cont.age)]
+pd.a[, type := 'Contribution to sexual contacts']
+
+dta <- readRDS(file.path('scripts', 'shift', 'gp_221201d-central-output-log_lambda_latentby_direction_round_age_transmission.source_age_infection.recipientstandardisedby_direction_round.rds')) # subset to round 18
+dta <- dta[ROUND == 'R018'] # subset to gender of the source (i.e.., Female sources or Male sources)
+setnames(dta, c('LABEL_GENDER_SOURCE', 'LABEL_GENDER_RECIPIENT', 'AGE_TRANSMISSION.SOURCE', 'AGE_INFECTION.RECIPIENT'), c('part.sex', 'cont.sex', 'part.age', 'cont.age'))
+dta <- dta[cont.age %in% c(15, 20, 25, 30, 35, 40)]
+dta <- dta[, label := ifelse(cont.age %in% c(15, 20, 25), '15, 20, 25 years', '30, 35, 40 years')]
+dta[, plt.age := cont.age]
+dta[, plt.bar := (part.age - cont.age) %in% c( -9, -4, 1,  6,  16)]
+dta[, type := 'Contribution to transmitting partners ']
+dta <- dta[, cont.age := ifelse(cont.age %in% c(15, 20, 25), paste0('Sources trainsmitting to ', cont.sex, ' aged ', cont.age), paste0('Sources trainsmitting to ', cont.sex, ' aged ', cont.age))]
+
+plt <- rbind(pd.a[, list(part.age,cont.age,CL,CU,M,part.sex,cont.sex,label, plt.bar, plt.age,type)],
+             dta[, list(part.age,cont.age,CL,CU,M,part.sex,cont.sex,label, plt.bar, plt.age,type)]
+             , use.names = TRUE, fill = TRUE)
+plt <- plt[order(label)]
+unique(plt$cont.age)
+plt$cont.age <- as.factor(plt$cont.age)
+
+
+# plot B----
+plt[,label := paste0(cont.sex, ' aged ', label)]
+# try to get all labels
+# if part.sex is Men, then change women's age 15 -> 30 etc
+plt[cont.sex == 'Men', plt.age := ifelse(plt.age %in% c(15, 20, 25), plt.age + 15, plt.age)]
+plt.xaxis <- ifelse(plt$part.sex == 'Men', 'Male', 'Female')
+p1b <- ggplot(plt[grepl('15', label)], aes(x = part.age, y = M, col = factor(plt.age, levels = unique(plt.age)), linetype = type)) +
+  geom_line(data =  plt[grepl('15', label)], aes(x = part.age, y = M)) +
+  #
+  geom_pointrange(data = plt[grepl('15', label) &  (plt.bar == TRUE )],
+                  aes(ymin = CL, ymax = CU), linetype = 1, size = .5, fatten = 2) +
+
+  facet_grid(.~paste0(label)) +
+  scale_x_continuous(breaks = seq(15, 70, 5)) +
+  scale_color_manual(values = c(
+    '#f46d43',
+    '#3288bd',
+    # '#5e4fa2',
+    '#88419d',
+    '#f46d43',
+    '#3288bd',
+    '#88419d'
+  )) +
+
+  scale_linetype_manual(values = c(2,1)) +
+  labs(x = 'Female age                                                               Male age',
+       y = paste0('Percent'),
+       col = 'Age of contact',
+       # fill = 'Age of contact',
+       linetype = 'type of contact'
+  ) +
+  guides(color = guide_legend(nrow = 1)) +
+  theme_bw() +
+  theme(
+    strip.background = element_blank(),
+    # strip.background = element_rect(colour="white", fill="white"),
+    legend.position = 'bottom',
+    legend.title = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    # panel.background = element_blank(),
+    panel.background = element_rect('white'),
+    panel.border = element_rect(color = "black",
+                                fill = NA),
+    axis.line = element_line(colour = "black"),
+    strip.text = element_text(size = 16),
+    legend.text = element_text(size = 9),
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 16) ) +
+  scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, .05)), limits = c(0,NA))
+
+p2b <- ggplot(plt[grepl('35', label)], aes(x = part.age, y = M, col = factor(plt.age, levels = unique(plt.age)), linetype = type)) +
+  geom_line(data =  plt[grepl('35', label)], aes(x = part.age, y = M)) +
+  #
+  geom_pointrange(data = plt[grepl('35', label) &  (plt.bar == TRUE )],
+                  aes(ymin = CL, ymax = CU), linetype = 1, size = .5, fatten = 2) +
+
+  facet_grid(.~paste0(label)) +
+  scale_x_continuous(breaks = seq(15, 70, 5)) +
+  scale_color_manual(values = c(
+    '#f46d43',
+    '#3288bd',
+    # '#5e4fa2',
+    '#88419d',
+    '#f46d43',
+    '#3288bd',
+    '#88419d'
+  )) +
+
+  scale_linetype_manual(values = c(2,1)) +
+  labs(x = 'Female age                                                               Male age',
+       y = paste0('Percent'),
+       col = 'Age of contact',
+       # fill = 'Age of contact',
+       linetype = 'type of contact'
+  ) +
+  guides(color = guide_legend(nrow = 1)) +
+  theme_bw() +
+  theme(
+    strip.background = element_blank(),
+    legend.position = 'bottom',
+    legend.title = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    # panel.background = element_blank(),
+    panel.background = element_rect('white'),
+    panel.border = element_rect(color = "black",
+                                fill = NA),
+    axis.line = element_line(colour = "black"),
+    strip.text = element_text(size = 16),
+    legend.text = element_text(size = 9),
+    axis.text = element_text(size = 14),
+    axis.title = element_text(size = 16) ) +
+  scale_y_continuous(labels = scales::percent, expand = expansion(mult = c(0, .05)), limits = c(0,NA))
+
+pB <- ggpubr::ggarrange(p1b, p2b, ncol = 1, common.legend = T, legend = 'bottom')
+pB
+
+
+# combine two plots----
+p <- ggpubr::ggarrange(pA, pB, ncol = 1, heights  = c(1.35, 2.8), labels = c('a', 'b'), font.label=list(color="black",size=16))
+p
+ggsave(file = file.path('results', 'shift', 'extended-data-fig_age-dist_0112.png'), p, width = 10.5, height = 13)
+ggsave(file = file.path('results', 'shift', 'extended-data-fig_age-dist_0112.pdf'), p, width = 10.5, height = 13)
