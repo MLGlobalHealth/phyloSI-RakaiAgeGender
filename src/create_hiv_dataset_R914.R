9367c2ab458aee8653d8678fd6c8c7dd4e99da81
Mélodie MONOD <monod.melodie@gmail.com>
2023-02-28 17:22:07 +0000

    find interview date

--- src/create_hiv_dataset_R914.R
+++ src/create_hiv_dataset_R914.R
@@ -11,10 +11,16 @@ file.quest_R09_R14 = file.path(indir.deepsequencedata, 'RCCS_R9_R14/quest_R09_R1
 quest_R09_R14 <- as.data.table(read.csv(file.quest_R09_R14))
 
 # select variable from hiv dataset
-hiv_R09_R14 <- select(quest_R09_R14, c('study_id', 'round', 'hivdate', 'hiv', 'revertor', 'id', 'lastnegv', 'lastnegvd', 
+hiv_R09_R14 <- select(quest_R09_R14, c('study_id', 'round', 'hivdate', 'int_date', 'hiv', 'revertor', 'id', 'lastnegv', 'lastnegvd', 
                                        'firstpos_diagnosis_vis', 'firstpos_diagnosis_dt'))
 hiv_R09_R14 <- unique(hiv_R09_R14)
+
+# find interview date  
+hiv_R09_R14[, int_date := as.Date(int_date, format = '%d/%m/%Y')]
+hiv_R09_R14[hivdate == '', hivdate := as.character(int_date)]
+hiv_R09_R14 <- select(hiv_R09_R14, -c('int_date'))
 
 # save
 file.hiv_R09_R14 = file.path(indir.deepsequencedata, 'RCCS_R9_R14/HIV_R09_R14.csv')
 write.csv(hiv_R09_R14, file = file.hiv_R09_R14, row.names = F)
+
